82b45f25f6d109e3c7eb4c5656dcae9ba4075aa1
zhangwei <zhangwei24@apache.org>
2022-02-28 16:59:35 +0800

    support async runtime

--- plugin/skywalking/test/binding-event/provider/provider.go
+++ plugin/skywalking/test/binding-event/provider/provider.go
@@ -10,6 +10,7 @@ import (
 	"github.com/OpenFunction/functions-framework-go/plugin/skywalking"
 	"github.com/SkyAPM/go2sky"
 	"k8s.io/klog/v2"
+	agentv3 "skywalking.apache.org/repo/goapi/collect/language/agent/v3"
 )
 
 func bindingsFunction(ofCtx ofctx.Context, in []byte) (ofctx.Out, error) {
@@ -23,6 +24,8 @@ func bindingsFunction(ofCtx ofctx.Context, in []byte) (ofctx.Out, error) {
 		ofCtx.GetInnerEvent().SetMetadata(headerKey, headerValue)
 		return nil
 	})
+	span.SetSpanLayer(agentv3.SpanLayer_FAAS)
+	span.SetComponent(5013)
 	if err != nil {
 		klog.Error(err)
 		return ofCtx.ReturnOnInternalError().WithData([]byte(err.Error())), err
