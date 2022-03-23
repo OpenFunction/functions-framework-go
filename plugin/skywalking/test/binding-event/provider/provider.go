5e32151e9ba22c2f1d4df7a1716413347b306ccd
laminar <fangtian@kubesphere.io>
2022-03-23 19:33:02 +0800

    Set the exit span before sending the payload to the target. (#45)
    Signed-off-by: laminar <fangtian@kubesphere.io>

--- plugin/skywalking/test/binding-event/provider/provider.go
+++ plugin/skywalking/test/binding-event/provider/provider.go
@@ -4,36 +4,17 @@ import (
 	"context"
 	"time"
 
+	"k8s.io/klog/v2"
+
 	ofctx "github.com/OpenFunction/functions-framework-go/context"
 	"github.com/OpenFunction/functions-framework-go/framework"
 	"github.com/OpenFunction/functions-framework-go/plugin"
 	"github.com/OpenFunction/functions-framework-go/plugin/skywalking"
-	"github.com/SkyAPM/go2sky"
-	"k8s.io/klog/v2"
-	agentv3 "skywalking.apache.org/repo/goapi/collect/language/agent/v3"
 )
 
 func bindingsFunction(ofCtx ofctx.Context, in []byte) (ofctx.Out, error) {
-	tracer := go2sky.GetGlobalTracer()
-	if tracer == nil {
-		klog.Warning("go2sky is not enabled")
-		return ofCtx.ReturnOnInternalError().WithData([]byte("go2sky is not enabled")), nil
-	}
 
-	span, err := tracer.CreateExitSpan(ofCtx.GetNativeContext(), "sample-topic", "sample-topic", func(headerKey, headerValue string) error {
-		ofCtx.GetInnerEvent().SetMetadata(headerKey, headerValue)
-		return nil
-	})
-	if err != nil {
-		klog.Error(err)
-		return ofCtx.ReturnOnInternalError().WithData([]byte(err.Error())), err
-	}
-	defer span.End()
-
-	span.SetSpanLayer(agentv3.SpanLayer_FAAS)
-	span.SetComponent(5013)
-
-	_, err = ofCtx.Send("sample-topic", []byte(time.Now().String()))
+	_, err := ofCtx.Send("sample-topic", []byte(time.Now().String()))
 	if err != nil {
 		klog.Error(err)
 		return ofCtx.ReturnOnInternalError().WithData([]byte(err.Error())), err
