a5eb89d132e0191a9f55ff2d066e106b05bbd694
Lize Cai <lize.cai@sap.com>
2022-05-19 13:54:22 +0800

    add tests for declarative functions
    Signed-off-by: Lize Cai <lize.cai@sap.com>

---
+++ test/declarative/sync-http-multiple/http.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/OpenFunction/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloWorld", helloWorld, functions.WithFunctionPath("/helloworld"))
+	functions.HTTP("Foo", foo, functions.WithFunctionPath("/foo"))
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"hello": "world",
+	}
+	responseBytes, _ := json.Marshal(response)
+	w.Header().Set("Content-type", "application/json")
+	w.Write(responseBytes)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"hello": "bar",
+	}
+	responseBytes, _ := json.Marshal(response)
+	w.Header().Set("Content-type", "application/json")
+	w.Write(responseBytes)
+}
---
+++ test/declarative/sync-http/verify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+url=http://$1
+while true; do
+  st=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$url")
+  if [ "$st" -eq 200 ]; then
+    data_result=$(curl -X GET -H "Content-type: application/json" -H "Accept: application/json" -s "$url" | yq -P ".")
+    plugin_result=$(KUBECONFIG=/tmp/e2e-k8s.config kubectl logs --tail=1 -l app="sync-http" -c http | awk '{ print $8 }' | yq -P '.' -)
+    break
+  else
+    sleep 1
+    continue
+  fi
+done
+
+echo "$data_result"
+echo "$plugin_result"
