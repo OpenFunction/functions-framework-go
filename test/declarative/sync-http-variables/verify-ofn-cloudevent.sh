0c56a302e771c5606b908ed8ccec902dac5c2cbe
Lize Cai <lize.cai@sap.com>
2022-06-19 12:15:49 +0800

    update tests
    Signed-off-by: Lize Cai <lize.cai@sap.com>

--- test/declarative/sync-http-variables/verify-ofn-cloudevent.sh
+++ test/declarative/sync-http-variables/verify-ofn-cloudevent.sh
@@ -4,8 +4,8 @@ url=http://$1
 while true; do
   st=$(curl -s -o /dev/null -w "%{http_code}" "$url" -H "Ce-Specversion: 1.0" -H "Ce-Type: io.openfunction.samples.helloworld" -H "Ce-Source: io.openfunction.samples/helloworldsource" -H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f79" -H "Content-Type: application/json" -d 'hello')
   if [ "$st" -eq 200 ]; then
-    data_result=$(KUBECONFIG=/tmp/e2e-k8s.config kubectl logs --tail=2 -l app="sync-http-variables" -c http | grep Data | awk '{ print $8 }' | yq -P '.' -)
-    plugin_result=$(KUBECONFIG=/tmp/e2e-k8s.config kubectl logs --tail=2 -l app="sync-http-variables" -c http | grep plugin | awk '{ print $8 }' | yq -P '.' -)
+    data_result=$(curl -X POST -H "Content-type: application/json" -H "Accept: application/json" -s "$url" -H "Ce-Specversion: 1.0" -H "Ce-Type: io.openfunction.samples.helloworld" -H "Ce-Source: io.openfunction.samples/helloworldsource" -H "Ce-Id: 536808d3-88be-4077-9d7a-a3f162705f79" -H "Content-Type: application/json" -d 'hello' | yq -P ".")
+    plugin_result=$(KUBECONFIG=/tmp/e2e-k8s.config kubectl logs --tail=1 -l app="sync-http-variables" -c http | grep plugin | awk '{ print $8 }' | yq -P '.' -)
     break
   else
     sleep 1
--- test/declarative/sync-http-variables/verify-ofn-http.sh
+++ test/declarative/sync-http-variables/verify-ofn-http.sh
@@ -4,7 +4,7 @@ url=http://$1
 while true; do
   st=$(curl -s -o /dev/null -w "%{http_code}" -X GET "$url")
   if [ "$st" -eq 200 ]; then
-    data_result=$(curl -X GET -H "Content-type: application/json" -H "Accept: application/json" -s "$url" | yq -P ".")
+    data_result=$(curl -X POST -H "Content-type: application/json" -H "Accept: application/json" -s "$url" -d 'hello' | yq -P ".")
     plugin_result=$(KUBECONFIG=/tmp/e2e-k8s.config kubectl logs --tail=1 -l app="sync-http-variables" -c http | awk '{ print $8 }' | yq -P '.' -)
     break
   else
